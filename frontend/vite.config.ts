858f79b135f694cc6f3196d1e3e465d4ad491fb0
Chinmay Sawant <sawantchinmay040@gmail.com>
2026-03-21 23:23:11 +0530

    fixing the github pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/deslop/',
   plugins: [react(), tailwindcss()],
   build: {
     outDir: '../docs',
